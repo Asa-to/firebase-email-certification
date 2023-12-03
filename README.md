d1b5dd643c3f037ec5d393e0300b70040eed70c7
Asa-to <sethuna03@gmail.com>
2023-12-03 22:05:21 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# React + TypeScript + Vite
+
+gcp で email 認証を行うテスト
+
+1. firebase でプロジェクトを作成
+2. firebase をプロジェクトに追加
+   1. pnpm add firebase
+   2. firebase の初期化処理を App.tsx に追加
+      1. firebaseConfig の設定
+3. firebase プロジェクトで authentication からメール認証(パスワードあり) を有効にする
---
+++ src/App.tsx
@@ -0,0 +1,126 @@
+import {
+  User,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import "./App.css";
+import { FirebaseError, initializeApp } from "firebase/app";
+import { useCallback, useEffect, useState } from "react";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+function App() {
+  const auth = getAuth(app);
+  const [userInfo, setUserInfo] = useState<User | null>(auth.currentUser);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSignIn, setIsSignIn] = useState(true);
+  const signUpOrSignIn = useCallback(
+    async (email: string, password: string, isSignIn = true) => {
+      try {
+        if (isSignIn) {
+          await signInWithEmailAndPassword(auth, email, password);
+        } else {
+          await createUserWithEmailAndPassword(auth, email, password);
+        }
+      } catch (error) {
+        const firebaseError = error as FirebaseError;
+        if (firebaseError.code === "auth/email-already-in-use") {
+          alert("使用されたemailは登録済みです");
+        } else {
+          alert(firebaseError.message);
+        }
+      }
+    },
+    [auth]
+  );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserInfo(user);
+      }
+    });
+  }, [auth]);
+
+  useEffect(() => {
+    setUserInfo(auth.currentUser);
+  }, [auth.currentUser]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "4px",
+      }}
+    >
+      {userInfo ? (
+        <div>
+          <p>
+            ようこそ{userInfo.displayName ?? userInfo.email ?? userInfo.uid}
+            さん
+          </p>
+          <button
+            onClick={async () => {
+              await auth.signOut();
+              setUserInfo(null);
+            }}
+          >
+            ログアウト
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>ログインしてください</p>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              signUpOrSignIn(email, password, isSignIn);
+            }}
+          >
+            <div>
+              <label htmlFor="email">メールアドレス</label>
+              <input
+                required
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">パスワード</label>
+              <input
+                required
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" onClick={() => setIsSignIn(false)}>
+              sign up
+            </button>
+            <button type="submit" onClick={() => setIsSignIn(true)}>
+              sign in
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
